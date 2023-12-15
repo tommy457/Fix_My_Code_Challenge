b13d5762769d222f5dd5e2f286c5d8593ceab7b8
tommy457 <boukhalfaml1011@gmail.com>
2023-12-15 12:18:23 -0800

    it\'s all about that PEP8 style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ Module for square class """
 
 
-class square:
+class Square():
     """ Square class """
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class square:
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
@@ -27,7 +27,7 @@ class square:
 
 if __name__ == "__main__":
     """Create square and print its area and permiter and its represantation"""
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
