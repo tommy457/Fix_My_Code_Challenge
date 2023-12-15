5876039fcb93bdb25eba43b1ee2e8576cabee3fe
tommy457 <boukhalfaml1011@gmail.com>
2023-12-15 12:21:08 -0800

    replaced width with height

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -14,7 +14,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiter_of_my_square(self):
         """ Permiter of the square """
